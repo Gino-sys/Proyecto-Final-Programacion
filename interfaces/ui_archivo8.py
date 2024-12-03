edf25e060b49fdbc25d32193912b4c3e2b3c09e6
nachofarah <nachofarah07@gmail.com>
2024-12-03 16:43:42 -0300

    Cambios en la ventana de podios

--- interfaces/ui_archivo8.py
+++ interfaces/ui_archivo8.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'archivo8LlhujN.ui'
+## Form generated from reading UI file 'archivo8JtHsdZ.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -11,7 +11,6 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
 
 
 class Ui_MainWindow(object):
@@ -25,29 +24,21 @@ class Ui_MainWindow(object):
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(-10, -50, 931, 551))
         self.label.setPixmap(QPixmap(u"diseños/Podioimg.png"))
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(20, 110, 311, 21))
-        font = QFont()
-        font.setFamily(u"Cooper Black")
-        font.setPointSize(22)
-        self.label_2.setFont(font)
-        self.label_2.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
         self.label_3.setGeometry(QRect(410, 170, 151, 51))
-        font1 = QFont()
-        font1.setFamily(u"Cooper Black")
-        font1.setPointSize(14)
-        font1.setBold(False)
-        font1.setWeight(50)
-        self.label_3.setFont(font1)
+        font = QFont()
+        font.setFamily(u"Cooper Black")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_3.setFont(font)
         self.label_3.setStyleSheet(u"color: white;\n"
 "")
         self.label_3.setAlignment(Qt.AlignCenter)
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(20, 160, 261, 61))
+        self.pushButton.setGeometry(QRect(20, 140, 261, 61))
         self.pushButton.setStyleSheet(u"background-color: #1e7e34;  /* Verde oscuro */\n"
 "color: white;               /* Texto en blanco */\n"
 "")
@@ -58,20 +49,20 @@ class Ui_MainWindow(object):
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
         self.label_4.setGeometry(QRect(270, 380, 121, 41))
-        self.label_4.setFont(font1)
+        self.label_4.setFont(font)
         self.label_4.setStyleSheet(u"color: white;\n"
 "")
         self.label_4.setAlignment(Qt.AlignCenter)
         self.label_5 = QLabel(self.centralwidget)
         self.label_5.setObjectName(u"label_5")
         self.label_5.setGeometry(QRect(570, 380, 121, 41))
-        self.label_5.setFont(font1)
+        self.label_5.setFont(font)
         self.label_5.setStyleSheet(u"color: white;\n"
 "")
         self.label_5.setAlignment(Qt.AlignCenter)
         self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(20, 230, 221, 61))
+        self.pushButton_2.setGeometry(QRect(20, 220, 221, 61))
         icon1 = QIcon()
         icon1.addFile(u"diseños/Salir.png", QSize(), QIcon.Normal, QIcon.Off)
         self.pushButton_2.setIcon(icon1)
@@ -94,7 +85,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Ventana8", None))
         self.label.setText("")
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"NOMBRE_TORNEO", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.pushButton.setText("")
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
